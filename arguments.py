5aa03c1c7137d2c6de180b27029071d257ef8f39
betaniarodrigues <betania_rodrigues1997@hotmail.com>
2024-08-29 11:18:32 +0000

    Files CPC

---
+++ arguments.py
@@ -0,0 +1,147 @@
+import argparse
+
+import os
+import torch
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Parameters for Contrastive Predictive Coding for Human "
+        "Activity Recognition"
+    )
+
+    # Data loading parameters
+    parser.add_argument("--window", type=int, default=50, help="Window size")
+    parser.add_argument(
+        "--overlap", type=int, default=25, help="Overlap between consecutive windows"
+    )
+
+    # Training settings
+    parser.add_argument("-b", "--batch_size", type=int, default=64)
+    parser.add_argument("--learning_rate", type=float, default=5e-4)
+    parser.add_argument("--num_epochs", type=int, default=150)
+    parser.add_argument("--gpu_id", type=str, default="0")
+
+    # Dataset to train on
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="mobiact",
+        help="Choosing the dataset to perform the training on",
+    )
+
+    # Conv encoder
+    parser.add_argument(
+        "--kernel_size",
+        type=int,
+        default=3,
+        help="Size of the conv filters in the encoder",
+    )
+
+    # Future prediction horizon
+    parser.add_argument(
+        "--num_steps_prediction",
+        type=int,
+        default=28,
+        help="Number of steps in the future to predict",
+    )
+
+    # ------------------------------------------------------------
+    # Classification parameters
+    parser.add_argument(
+        "--classifier_lr",
+        type=float,
+        default=5e-4,
+    )
+    parser.add_argument("--classifier_batch_size", type=int, default=256)
+    parser.add_argument(
+        "--saved_model",
+        type=str,
+        default=None,
+        help="Full path of the learned CPC model",
+    )
+    parser.add_argument(
+        "--learning_schedule",
+        type=str,
+        default="last_layer",
+        choices=["last_layer", "all_layers"],
+        help="last layer freezes the encoder weights but " "all_layers does not.",
+    )
+    # ------------------------------------------------------------
+
+    # Random seed for reproducibility
+    parser.add_argument("--random_seed", type=int, default=42)
+
+    parser.add_argument(
+        "--data_percentage",
+        type=int,
+        default=100,
+        help="Percentage of data to use for training (default: 100)",
+    )
+
+    args = parser.parse_args()
+
+    # Setting parameters by the dataset
+
+    args.root_dir = "../CPC/"
+    args.input_size = 6
+
+    if args.dataset == "UCI_raw_12":
+        args.data_file = "UCI_raw_12"
+        args.num_classes = 13
+    elif args.dataset == "UCI_raw":
+        args.data_file = "UCI_raw"
+        args.num_classes = 7
+    elif args.dataset == "MotionSense_raw":
+        args.data_file = "MotionSense_raw"
+        args.num_classes = 6
+    elif args.dataset == "KuHar_raw":
+        args.data_file = "KuHar_raw"
+        args.num_classes = 18
+    elif args.dataset == "RealWorld_raw":
+        args.data_file = "RealWorld_raw"
+        args.num_classes = 9
+    elif args.dataset == "KuHar_MotionSense":
+        args.data_file = "KuHar_MotionSense"
+        args.num_classes = 18
+    elif args.dataset == "UCI_KuHar":
+        args.data_file = "UCI_KuHar"
+        args.num_classes = 18
+    elif args.dataset == "UCI_raw_g":
+        args.data_file = "UCI_raw_g"
+        args.num_classes = 7
+    elif args.dataset == "UCI_MotionSense_KuHar_RealWorld":
+        args.data_file = "UCI_MotionSense_KuHar_RealWorld"
+        args.num_classes = 18
+    elif args.dataset == "KuHar_RealWorld":
+        args.data_file = "KuHar_RealWorld"
+        args.num_classes = 18
+    elif args.dataset == "UCI_MotionSense":
+        args.data_file = "UCI_MotionSense"
+        args.num_classes = 13
+    elif args.dataset == "UCI_RealWorld":
+        args.data_file = "UCI_RealWorld"
+        args.num_classes = 9
+    elif args.dataset == "MotionSense_RealWorld":
+        args.data_file = "MotionSense_RealWorld"
+        args.num_classes = 9
+    elif args.dataset == "KuHar_UCI_RealWorld":
+        args.data_file = "KuHar_UCI_RealWorld"
+        args.num_classes = 18
+    elif args.dataset == "KuHar_MotionSense_RealWorld":
+        args.data_file = "KuHar_MotionSense_RealWorld"
+        args.num_classes = 18
+    elif args.dataset == "UCI_MotionSense_RealWorld":
+        args.data_file = "UCI_MotionSense_RealWorld"
+        args.num_classes = 13
+
+    args.device = torch.device(
+        "cuda:" + str(args.gpu_id) if torch.cuda.is_available() else "cpu"
+    )
+
+    # Conv padding size
+    args.padding = int(args.kernel_size // 2)
+
+    return args
---
+++ dataset.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+from pathlib import Path
+import pandas as pd
+from sliding_window import sliding_window
+
+
+def opp_sliding_window(data_x, data_y, ws, ss):
+
+    data_x = sliding_window(data_x, (ws, data_x.shape[1]), (ss, 1))
+
+    # Just making it a vector if it was a 2D matrix
+    data_y = np.reshape(data_y, (len(data_y),))
+    data_y = np.asarray([[i[-1]] for i in sliding_window(data_y, ws, ss)])
+    return data_x.astype(np.float32), data_y.reshape(len(data_y)).astype(np.uint8)
+
+
+# Defining the data loader for the implementation
+class HARDataset(Dataset):
+    def __init__(self, args, phase):
+        self.filename = os.path.join(args.root_dir, args.data_file)
+
+        self.data_raw = self.load_dataset()
+        assert args.input_size == self.data_raw[phase]["data"].shape[1]
+        print("Data Raw", self.data_raw[phase]["data"].shape)
+        # Obtaining the segmented data
+        self.data, self.labels = opp_sliding_window(
+            self.data_raw[phase]["data"],
+            self.data_raw[phase]["labels"],
+            args.window,
+            args.overlap,
+        )
+        print("Data", self.data.shape)
+        print("Labels", self.labels.shape)
+
+    # Load .csv file
+
+    def load_dataset(self):
+
+        data_path = Path(self.filename)
+
+        datasets = {}
+
+        for phase in ["train", "val", "test"]:
+
+            phase_path = data_path / phase
+
+            datas_x = []
+
+            data_y = []
+
+            for f in phase_path.glob("*.csv"):
+
+                data = pd.read_csv(f)
+
+                x = data[
+                    ["accel-x", "accel-y", "accel-z", "gyro-x", "gyro-y", "gyro-z"]
+                ].values
+
+                datas_x.append(x)
+
+                y = data["activity code"].values
+
+                data_y.append(y)
+
+            datasets[phase] = {
+                "data": np.concatenate(datas_x),
+                "labels": np.concatenate(data_y),
+            }
+            datasets[phase]["data"] = datasets[phase]["data"].astype(np.float32)
+            datasets[phase]["labels"] = datasets[phase]["labels"].astype(np.uint8)
+
+        return datasets
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        data = self.data[index, :, :]
+
+        data = torch.from_numpy(data).double()
+
+        label = torch.from_numpy(np.asarray(self.labels[index])).double()
+        return data, label
+
+
+def load_dataset(args, classifier=False):
+    datasets = {x: HARDataset(args=args, phase=x) for x in ["train", "val", "test"]}
+
+    def get_batch_size():
+        if classifier:
+            batch_size = args.classifier_batch_size
+        else:
+            batch_size = args.batch_size
+
+        return batch_size
+
+    # Get a subset of the dataset
+    def get_subset(dataset, data_percentage):
+        subset_size = int(len(dataset) * (data_percentage / 100.0))
+        indices = np.random.choice(len(dataset), subset_size, replace=False)
+        return Subset(dataset, indices)
+
+    data_loaders = {
+        x: DataLoader(
+            (
+                get_subset(datasets[x], args.data_percentage)
+                if x == "train"
+                else datasets[x]
+            ),
+            batch_size=get_batch_size(),
+            shuffle=True if x == "train" else False,
+            num_workers=2,
+            pin_memory=True,
+        )
+        for x in ["train", "val", "test"]
+    }
+
+    # Printing the batch sizes
+    for phase in ["train", "val", "test"]:
+        print(
+            "The batch size for {} phase is: {}".format(
+                phase, data_loaders[phase].batch_size
+            )
+        )
+
+    dataset_sizes = {x: len(datasets[x]) for x in ["train", "val", "test"]}
+
+    print("Dataset sizes:", dataset_sizes)
+    for phase in ["train", "val", "test"]:
+        print(f"Dataset size for {phase}: {dataset_sizes[phase]}")
+
+    return data_loaders, dataset_sizes
